e7b67412ad79aa4f287bc29aeb8054c5f0885e89
sebbratt1 <43443754+sebbratt1@users.noreply.github.com>
2019-11-14 14:54:26 +0000

    more update

--- MobileOutputTask/platforms/android/platform_www/cordova_plugins.js
+++ MobileOutputTask/platforms/android/platform_www/cordova_plugins.js
@@ -19,6 +19,7 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
   ];
   module.exports.metadata = {
     "cordova-plugin-dialogs": "2.0.2",
-    "cordova-plugin-whitelist": "1.3.4"
+    "cordova-plugin-whitelist": "1.3.4",
+    "cordova-plugin-vibration": "3.1.1"
   };
 });
--- MobileOutputTask/www/js/index.js
+++ MobileOutputTask/www/js/index.js
@@ -5,13 +5,13 @@ function randomResult()
 	
 	if(randomValue == 1)
 	{
-		navigator.notification.beep(1);	//beep once
-		window.alert("working");
+		navigator.notification.beep(1);
+		window.alert("true");
 	}
 	else
 	{
-		navigator.notification.beep(2);	//beep twice
-		window.alert("working");
+		navigator.vibrate([200, 300, 500]);
+		window.alert("false");
 	}
 	
 
